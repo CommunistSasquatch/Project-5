c9d735677c048124f62f66d977f09d34279ef7fe
Joe B <mothman147@gmail.com>
2015-11-06 15:12:09 -0500

    added stuff

--- ApgarMedical.pl
+++ ApgarMedical.pl
@@ -6,40 +6,82 @@
 use 5.14.1;
 use warnings;
 
-my (@data);
+my (@data, @years);
 use constant BESTDATAIN => "./ApgarMedicalBest.txt";
 use constant CUSHINGDATAIN => "./ApgarMedicalCushing.txt";
+use constant SORTEDDATAOUT => "./ApgarMedicalSORTED.txt";
 use constant COLUMNS => 3;
 
 sub main {
 	@data = ([0],[0]);
-	dataIn();
+	@years = ([0],[0]);
+	readDataIn();
+	populateYears();
 	printData();
+	#findYear();
+	dataOut();
+	
 }
 
 main();
 
-sub dataIn {
+sub populateYears {
+	@years = ();
+	my $size = @years;
+	for (my $i = 0; $i < $size; $i++){
+		$years[$i][0] = $data [$i][COLUMNS];
+	}
+}
+
+sub findYear {
+	use constant YEAR_BORN => 3;
+	my $size = @data;
+	for (my $i = 0; $i < $size; $i++) {
+		if ($data[$i][YEAR_BORN] == $years[$i][0]){
+			$years[$i][1]++;
+		}
+	}
+}
+
+sub dataOut {
+	my $size = @data;
+	my $OUT;
+	open ($OUT, ">>", SORTEDDATAOUT);
+	for (my $i = 0; $i < $size; $i++) {
+		print ($OUT $data[$i]);
+	}
+	close ($OUT);
+}
+
+
+sub readDataIn {
 	my $IN;
-	@data = ();
 	my $counter = 0;
+	my @tempData = ();
+	@data = ();
 	open ($IN, '<', BESTDATAIN);
 	while (<$IN>) {
-		chomp ($data[$counter] = $_);
+		@tempData = split(/,/);
+		for (my $i = 0; $i < COLUMNS; $i++) {
+			chomp ($data[$counter][$i] = $tempData[$i]);
+		}
 		$counter++;
 	}
 	close $IN;
-	open ($IN,'<', CUSHINGDATAIN);
+	open ($IN, '<', BESTDATAIN);
 	while (<$IN>) {
-		chomp ($data[$counter] = $_);
-		$counter++;
+		@tempData = split(/,/);
+		for (my $i = 0; $i < COLUMNS; $i++) {
+			chomp ($data[$counter][$i] = $tempData[$i]);
+		}
 	}
 }
 
 sub printData {
+	use constant ONE => 1;
 	my $size = @data;
 	for (my $i = 0; $i < $size; $i++) {
-		print "\t$data[$i]\n";
+		print "\t$data[$i][2]\n";
 	}
 }
 
